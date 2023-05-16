90a5e30c811b3e7d16fabba983276c5bed02da97
Enric Calabuig <ecalabuig@bmat.com>
2023-05-16 18:57:53 +0200

    Improve initialisation so it's fully managed by the lib

--- echo/src/bin/echo.rs
+++ echo/src/bin/echo.rs
@@ -1,98 +1,57 @@
-use std::io::StdoutLock;
-
-use anyhow::{self, bail, Context};
 use echo::{Body, Message, Node, Payload};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
 #[serde(tag = "type")]
-pub enum Request {
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    Echo {
-        echo: serde_json::Value,
-    },
+pub enum EchoRequest {
+    Echo { echo: serde_json::Value },
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
 #[serde(tag = "type")]
-pub enum Response {
-    InitOk,
+pub enum EchoResponse {
     EchoOk { echo: serde_json::Value },
 }
 
 #[derive(Debug)]
-pub struct EchoNode {
-    name: Option<String>,
+struct EchoNode {
     msg_id: usize,
 }
 
-impl Node<Request, Response> for EchoNode {
-    fn init(
-        &mut self,
-        msg: Message<Request, Response>,
-        output: &mut StdoutLock,
-    ) -> anyhow::Result<()> {
-        let payload = &msg.body.payload;
-        match &payload {
-            Payload::Request(Request::Init { node_id, .. }) => {
-                self.name = Some(node_id.clone());
-                self.reply(msg, output)
-                    .context("Failed to reply to init message")?;
-                Ok(())
-            }
-            _ => bail!("Tried to initialise node with wrong payload variant"),
-        }
+impl Node<(), EchoRequest, EchoResponse> for EchoNode {
+    fn from_init(_state: (), _init: echo::Init) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(EchoNode { msg_id: 1 })
     }
 
     fn create_reply(
         &mut self,
-        msg: Message<Request, Response>,
-    ) -> anyhow::Result<Message<Request, Response>> {
-        let reply: Message<Request, Response>;
+        msg: Message<EchoRequest, EchoResponse>,
+    ) -> anyhow::Result<Message<EchoRequest, EchoResponse>> {
         let request = msg
             .body
             .payload
             .request()
             .ok_or(anyhow::anyhow!("Message payload is not a request"))?;
-        match request {
-            Request::Init { node_id, .. } => {
-                self.name = Some(node_id);
-                reply = Message {
-                    src: self.name.clone().unwrap(),
-                    dest: msg.src.clone(),
-                    body: Body {
-                        msg_id: None,
-                        in_reply_to: msg.body.msg_id,
-                        payload: Payload::Response(Response::InitOk),
-                    },
-                };
-            }
-            Request::Echo { echo } => {
-                reply = Message {
-                    src: self.name.clone().unwrap(),
-                    dest: msg.src.clone(),
-                    body: Body {
-                        msg_id: Some(self.msg_id),
-                        in_reply_to: msg.body.msg_id,
-                        payload: Payload::Response(Response::EchoOk { echo }),
-                    },
-                };
-            }
-        }
+        let EchoRequest::Echo { echo } = request;
+        let reply: Message<EchoRequest, EchoResponse> = Message {
+            src: msg.dest,
+            dest: msg.src,
+            body: Body {
+                msg_id: None, //Some(self.msg_id),
+                in_reply_to: msg.body.msg_id,
+                payload: Payload::Response(EchoResponse::EchoOk { echo }),
+            },
+        };
         self.msg_id += 1;
         Ok(reply)
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    let node = EchoNode {
-        name: None,
-        msg_id: 0,
-    };
-    echo::run(node)
+    echo::run::<_, EchoNode, _, _>(())
 }
--- echo/src/lib.rs
+++ echo/src/lib.rs
@@ -1,4 +1,4 @@
-use std::io::{StdoutLock, Write};
+use std::io::{BufRead, StdoutLock, Write};
 
 use anyhow::{self, Context};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
@@ -12,7 +12,9 @@ pub struct Message<Req, Res> {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Body<Req, Res> {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub msg_id: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub in_reply_to: Option<usize>,
     #[serde(flatten)]
     pub payload: Payload<Req, Res>,
@@ -25,6 +27,26 @@ pub enum Payload<Req, Res> {
     Response(Res),
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum InitRequest {
+    Init(Init),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum InitResponse {
+    InitOk,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Init {
+    pub node_id: String,
+    pub node_ids: Vec<String>,
+}
+
 impl<Req, Res> Payload<Req, Res> {
     pub fn request(self) -> Option<Req> {
         match self {
@@ -34,46 +56,97 @@ impl<Req, Res> Payload<Req, Res> {
     }
 }
 
-pub trait Node<Req, Res>
+pub trait Node<S, Req, Res>
 where
     Req: Serialize + DeserializeOwned,
     Res: Serialize + DeserializeOwned,
 {
-    fn init(&mut self, msg: Message<Req, Res>, output: &mut StdoutLock) -> anyhow::Result<()>;
+    fn from_init(state: S, init: Init) -> anyhow::Result<Self>
+    where
+        Self: Sized;
+
     fn create_reply(&mut self, msg: Message<Req, Res>) -> anyhow::Result<Message<Req, Res>>;
 
     fn reply(&mut self, msg: Message<Req, Res>, output: &mut StdoutLock) -> anyhow::Result<()> {
         let reply = self.create_reply(msg)?;
-        serde_json::to_writer(&mut *output, &reply).context("Writing to stdout")?;
-        output
-            .write_all(b"\n")
-            .context("Writing newline to stdout")?;
+        serde_json::to_writer(&mut *output, &reply)?;
+        output.write_all(b"\n")?;
         Ok(())
     }
 }
 
-pub fn run<S, Req, Res>(mut node: S) -> anyhow::Result<()>
+pub fn run<S, N, Req, Res>(init_state: S) -> anyhow::Result<()>
 where
-    S: Node<Req, Res>,
+    N: Node<S, Req, Res>,
     Res: Serialize + DeserializeOwned,
     Req: Serialize + DeserializeOwned,
 {
     let stdin = std::io::stdin().lock();
+    let mut stdin = stdin.lines();
     let mut stdout = std::io::stdout().lock();
+    // let mut stderr = std::io::stderr().lock();
 
-    let mut stdin_messages =
-        serde_json::Deserializer::from_reader(stdin).into_iter::<Message<Req, Res>>();
-    let init_msg = stdin_messages.next();
+    let init_msg: Message<InitRequest, InitResponse> = serde_json::from_str(
+        &stdin
+            .next()
+            .expect("init message must be present")
+            .context("failed to read init message from stdin")?,
+    )
+    .context("couldn't deserialize init message")?;
 
-    let init_msg = init_msg
-        .expect("init message must be present")
-        .context("could not deserialize init message")?;
+    let Payload::Request(InitRequest::Init(init)) = init_msg.body.payload else {
+        panic!("first message should be init");
+    };
 
-    node.init(init_msg, &mut stdout)
-        .context("error initialising node")?;
-    for msg in stdin_messages {
-        let msg = msg.context("Couldn't deserialize message")?;
+    let mut node: N = Node::from_init(init_state, init).context("Couldn't initialise node")?;
+
+    let reply = Message::<InitRequest, InitResponse> {
+        src: init_msg.dest,
+        dest: init_msg.src,
+        body: Body {
+            msg_id: None,
+            in_reply_to: init_msg.body.msg_id,
+            payload: Payload::<InitRequest, InitResponse>::Response(InitResponse::InitOk),
+        },
+    };
+    serde_json::to_writer(&mut stdout, &reply).context("error writing message stdout")?;
+    stdout
+        .write_all(b"\n")
+        .context("error writing newline to stdout")?;
+
+    for msg in stdin {
+        let msg = msg.expect("error reading from stdin");
+        let msg: Message<Req, Res> =
+            serde_json::from_str(&msg).context("Couldn't deserialize message")?;
         node.reply(msg, &mut stdout)?;
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_init_serialisation() {
+        let message = Message {
+            src: String::from("source"),
+            dest: String::from("destination"),
+            body: Body {
+                msg_id: Some(0),
+                in_reply_to: None,
+                payload: Payload::<InitRequest, InitResponse>::Request(InitRequest::Init(Init {
+                    node_id: String::from("n0"),
+                    node_ids: vec![String::from("n0")],
+                })),
+            },
+        };
+        let serialized = serde_json::to_string(&message).unwrap();
+        assert_eq!(
+            serialized,
+            String::from(
+                r#"{"src":"source","dest":"destination","body":{"msg_id":0,"type":"init","node_id":"n0","node_ids":["n0"]}}"#
+            )
+        );
+    }
 }
