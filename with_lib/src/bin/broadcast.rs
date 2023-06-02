c1998df9fc8d938dbe9c297247b60f685bda28cb
Enric Calabuig <ecalabuig@bmat.com>
2023-06-02 23:12:15 +0200

    Broadcast: multi-node with lib working with gossiping
    Lesson learnt: test the deserialization of all messages that need to be
    handled. I was going crazy trying to make the periodic gossiping work
    and the problem was that that arm was never running as the match with
    Init never happened.

--- with_lib/src/bin/broadcast.rs
+++ with_lib/src/bin/broadcast.rs
@@ -1,26 +1,25 @@
 use async_trait::async_trait;
+use log::info;
 use maelstrom::protocol::Message;
 use maelstrom::{done, Node, Result, Runtime};
 use serde::{Deserialize, Serialize};
-use std::sync::{Arc, Mutex};
+use std::collections::HashSet;
+use std::sync::{Arc, Mutex, MutexGuard};
+use std::time::Duration;
 
 pub(crate) fn main() -> Result<()> {
     Runtime::init(try_main())
 }
 
 async fn try_main() -> Result<()> {
+    let runtime = Runtime::new();
     let handler = Arc::new(Handler::default());
-    Runtime::new().with_handler(handler).run().await
+    runtime.with_handler(handler).run().await
 }
 
 #[derive(Clone, Default)]
 struct Handler {
-    inner: Arc<Mutex<Inner>>,
-}
-
-#[derive(Clone, Default, Debug)]
-struct Inner {
-    set: std::collections::HashSet<u64>,
+    set: Arc<Mutex<std::collections::HashSet<u64>>>,
 }
 
 #[async_trait]
@@ -29,33 +28,57 @@ impl Node for Handler {
         let body: Result<RequestBody> = req.body.as_obj();
         match body {
             Ok(RequestBody::Broadcast { message }) => {
-                {
-                    //I don't fully understand why inner has to be dropped before
-                    //returning a Future
-                    let mut inner = self.inner.lock().unwrap();
-                    inner.set.insert(message.clone());
-                }
-                let resp = ResponseBody::BroadcastOk;
-                return runtime.reply(req, resp).await;
+                self.set.lock().unwrap().insert(message.clone());
+                return runtime.reply_ok(req).await;
             }
             Ok(RequestBody::Read) => {
-                let messages = self.inner.lock().unwrap().set.clone();
                 let resp = ResponseBody::ReadOk {
-                    messages: Vec::from_iter(messages),
+                    messages: Vec::from_iter(self.set.lock().unwrap().iter().copied()),
                 };
                 return runtime.reply(req, resp).await;
             }
             Ok(RequestBody::Topology { .. }) => {
                 return runtime.reply_ok(req).await;
             }
+            Ok(RequestBody::Init { .. }) => {
+                // spawn into tokio (instead of runtime) to not to wait
+                // until it is completed, as it will never be.
+                let (r0, h0) = (runtime.clone(), self.clone());
+                tokio::spawn(async move {
+                    loop {
+                        tokio::time::sleep(Duration::from_millis(500)).await;
+                        info!("emit replication signal");
+                        let s = h0.set.lock().unwrap();
+                        for n in r0.neighbours() {
+                            let msg = RequestBody::Gossip {
+                                messages: to_seq(&s),
+                            };
+                            drop(r0.send_async(n, msg));
+                        }
+                    }
+                });
+                return Ok(());
+            }
+            Ok(RequestBody::Gossip { messages }) => {
+                self.set.lock().unwrap().extend(messages.clone());
+                return Ok(());
+            }
             _ => done(runtime, req),
         }
     }
 }
 
-#[derive(Deserialize)]
+fn to_seq(s: &MutexGuard<HashSet<u64>>) -> Vec<u64> {
+    s.iter().copied().collect()
+}
+
+#[derive(Serialize, Deserialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
 enum RequestBody {
+    Init {
+        node_id: String,
+        node_ids: Vec<String>,
+    },
     Broadcast {
         message: u64,
     },
@@ -63,27 +86,20 @@ enum RequestBody {
     Topology {
         topology: std::collections::HashMap<String, Vec<String>>,
     },
+    Gossip {
+        messages: Vec<u64>,
+    },
 }
 
 #[derive(Serialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
 enum ResponseBody {
-    BroadcastOk,
     ReadOk { messages: Vec<u64> },
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn serialize_broadcast_ok() {
-        let body = ResponseBody::BroadcastOk;
-        assert_eq!(
-            "{\"type\":\"broadcast_ok\"}",
-            serde_json::to_string::<ResponseBody>(&body).unwrap()
-        )
-    }
 
     #[test]
     fn serialize_read_ok() {
