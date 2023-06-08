b9dd7ec86e8f6c1e3458519fc107de1a8fa81085
Enric Calabuig <ecalabuig@bmat.com>
2023-06-08 15:30:51 +0200

    Kafka: not working just yet. Single-threaded node first

--- Makefile
+++ Makefile
@@ -24,3 +24,13 @@ broadcast-latency-partitioned:
 
 grow-only-counter:
 	maelstrom/maelstrom test -w g-counter --bin with_lib/target/debug/g-counter --node-count 3 --rate 100 --time-limit 20 --nemesis partition
+	
+# kafka-style-log:
+# 	maelstrom/maelstrom test -w kafka --bin with_lib/target/debug/kafka --node-count 1 --concurrency 2n --time-limit 20 --rate 1000
+	
+kafka-style-log:
+	maelstrom/maelstrom test -w kafka --bin with_lib/target/debug/kafka --node-count 1 --concurrency 2n --time-limit 5 --rate 50
+
+kafka-style-log-single-threaded:
+	maelstrom/maelstrom test -w kafka --bin from_scratch/target/debug/kafka --node-count 1 --concurrency 2n --time-limit 5 --rate 50
+
---
+++ from_scratch/src/bin/kafka.rs
@@ -0,0 +1,134 @@
+use std::{collections::HashMap, io::Write};
+
+use gossip_glomers::{Body, Event, Message, MessageId, Node, Payload};
+use serde::{Deserialize, Serialize};
+use log::info;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "snake_case")]
+#[serde(tag = "type")]
+pub enum KafkaRequest {
+    Send { key: String, msg: usize },
+    Poll { offsets: HashMap<String, usize> },
+    CommitOffsets { offsets: HashMap<String, usize> },
+    ListCommittedOffsets { keys: Vec<String> },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "snake_case")]
+#[serde(tag = "type")]
+pub enum KafkaResponse {
+    SendOk {
+        offset: usize,
+    },
+    PollOk {
+        msgs: HashMap<String, Vec<[usize; 2]>>,
+    },
+    CommitOffsetsOk,
+    ListCommittedOffsetsOk {
+        offsets: HashMap<String, usize>,
+    },
+}
+
+#[derive(Debug)]
+struct KafkaNode {
+    node_id: String,
+    msg_id: MessageId,
+    logs: HashMap<String, Vec<usize>>,
+    committed_offsets: HashMap<String, usize>,
+}
+
+impl Node<(), KafkaRequest, KafkaResponse> for KafkaNode {
+    fn from_init(
+        _state: (),
+        init: gossip_glomers::Init,
+        _: std::sync::mpsc::Sender<Event<KafkaRequest, KafkaResponse, ()>>,
+    ) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(KafkaNode {
+            node_id: init.node_id,
+            msg_id: 1,
+            logs: HashMap::new(),
+            committed_offsets: HashMap::new(),
+        })
+    }
+
+    fn step(
+        &mut self,
+        msg: Event<KafkaRequest, KafkaResponse, ()>,
+        output: &mut dyn Write,
+    ) -> anyhow::Result<()> {
+        let Event::Message(msg) = msg else {
+            panic!("received unexpected injected variant");
+        };
+        let request = msg
+            .body
+            .payload
+            .request()
+            .ok_or(anyhow::anyhow!("Message payload is not a request"))?;
+        let reply_payload = match request {
+            KafkaRequest::Send { key, msg } => {
+                info!("{}, {}", key, msg);
+                self.logs
+                    .entry(key.clone())
+                    .or_insert_with(|| Vec::new())
+                    .push(msg);
+                info!("{:?}", self.logs);
+                let offset = self.logs.get(&key).unwrap().len();
+                info!("{}", offset);
+                Payload::Response(KafkaResponse::SendOk { offset })
+            }
+            KafkaRequest::Poll { offsets } => {
+                info!("Got poll message");
+                let mut msgs: HashMap<String, Vec<[usize; 2]>> = HashMap::new();
+                info!("{:?}", msgs);
+                for (key, offset) in offsets.iter() {
+                    let mut pairs: Vec<[usize; 2]> = Vec::new();
+                    if let Some(msgs2) = self.logs.get(key) {
+                        for (i, &msg) in msgs2.iter().skip(*offset).enumerate() {
+                            pairs.push([msg, offset + i]);
+                        }
+                        msgs.insert(key.clone(), pairs);
+                    } else {
+                        info!("Polled for non-existing key {}", key)
+                    }
+                }
+                info!("{:?}", msgs);
+                Payload::Response(KafkaResponse::PollOk { msgs })
+            }
+            KafkaRequest::CommitOffsets { offsets } => {
+                self.committed_offsets.extend(offsets);
+                Payload::Response(KafkaResponse::CommitOffsetsOk)
+            }
+            KafkaRequest::ListCommittedOffsets { keys } => {
+                let offsets = self
+                    .committed_offsets
+                    .iter()
+                    .filter(|(k, _)| keys.contains(k))
+                    .map(|(k, v)| (k.clone(), *v))
+                    .collect();
+                Payload::Response(KafkaResponse::ListCommittedOffsetsOk { offsets })
+            }
+        };
+
+        self.msg_id += 1;
+        let reply: Message<KafkaRequest, KafkaResponse> = Message {
+            src: self.node_id.clone(),
+            dest: msg.src,
+            body: Body {
+                msg_id: None,
+                in_reply_to: msg.body.msg_id,
+                payload: reply_payload,
+            },
+        };
+        serde_json::to_writer(&mut *output, &reply)?;
+        output.write_all(b"\n")?;
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    gossip_glomers::run::<_, KafkaNode, _, _, _>(())
+}
